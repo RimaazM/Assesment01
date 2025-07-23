7f82d4722b26048e5386ad6631b3268edb19e7fd
RimaazM <mrimaaz26@gmail.com>
2025-07-24 03:18:47 +1100

    Implemented bookable interface in Appointment class

--- src/assessment01/Appointment.java
+++ src/assessment01/Appointment.java
@@ -1,10 +1,11 @@
 package assessment01;
 // the appointment class represent a patients appointment with a health professional
-public class Appointment {
+public class Appointment implements bookable {
     private String name; //patient name
     private String mobile; // patient mobile number
     private String timeslot; // time slots which they have booked the appointment
     private HealthProfessional doctor; // the doctor or dietitian assigned to the appointment
+
 
     //constructor to create an appointment with the given details
     public Appointment(String name, String mobile, String timeslot, HealthProfessional doctor) {
@@ -24,5 +25,9 @@ public class Appointment {
         System.out.println("Mobile: " + mobile);
         System.out.println("Time: " + timeslot);
         System.out.println("Doctor: " + doctor.getName() + " (" + doctor.getDepartment() + ")");
+    }
+    @Override
+    public void book() {
+        System.out.println("Booking confirmed for: " + name);
     }
 }
