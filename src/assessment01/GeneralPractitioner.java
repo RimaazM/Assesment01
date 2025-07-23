ab961c160bf6d3cee1f20e781e835e679f924739
RimaazM <mrimaaz26@gmail.com>
2025-07-24 02:13:20 +1100

    Created GeneralPractitioner and Dietitian as child classes with unique fields and overridden printDetails() methods.

--- src/assessment01/GeneralPractitioner.java
+++ src/assessment01/GeneralPractitioner.java
@@ -1,17 +1,22 @@
 package assessment01;
 
+//the General Practitioner class represents a doctor who specializes in general medicine
+// extends the health professional class and includes a specific speciality field.
 public class GeneralPractitioner extends HealthProfessional {
+
+    //the specific area of focus for this general practitioner
     private String specialty;
 
+    // constructor to initialize a general practitioner object with its attributes
     public GeneralPractitioner(int id, String name, String department, String specialty) {
-        super(id, name, department);
+        super(id, name, department); // inherit common attributes for health professional
         this.specialty = specialty;
     }
-
+    //prints the general practitioner full details including inherited and unique filed
     @Override
     public void printDetails() {
         System.out.println("Type: General Practitioner");
-        super.printDetails();
+        super.printDetails(); //print id, name and department
         System.out.println("Specialty: " + specialty);
     }
 }
