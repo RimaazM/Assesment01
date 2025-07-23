ed79f1bd9b642a2c3876cdd3f553debf12d65b6d
RimaazM <mrimaaz26@gmail.com>
2025-07-24 02:16:15 +1100

    Added Dietitian class with certification field and comments

--- src/assessment01/Dietitian.java
+++ src/assessment01/Dietitian.java
@@ -1,13 +1,18 @@
 package assessment01;
 
+// the dietitian class represent a health professional specialized in nutrition.
+// it extends the health professional base class.
+
 public class Dietitian extends HealthProfessional {
+    //certification level oof dietitian
     private String certification;
 
+    //constructor to initialize a dietitian object with specific attributes
     public Dietitian(int id, String name, String department, String certification) {
-        super(id, name, department);
+        super(id, name, department); //calls constructors from the parent class
         this.certification = certification;
     }
-
+// print the details of dietitian including inherited attributes and certification
     @Override
     public void printDetails() {
         System.out.println("Type: Dietitian");
