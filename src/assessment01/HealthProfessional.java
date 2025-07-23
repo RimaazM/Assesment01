c1610ddff66cfb110d6313b9bd66c65c636ddf79
RimaazM <mrimaaz26@gmail.com>
2025-07-24 02:12:19 +1100

    created HealthProfessional  as the parent class with shared fields and methods

--- src/assessment01/HealthProfessional.java
+++ src/assessment01/HealthProfessional.java
@@ -1,26 +1,28 @@
 package assessment01;
 
 public class HealthProfessional {
-    private int id;
-    private String name;
-    private String department;
+    private int id;//unique id to identify the health professional
+    private String name;// full name of thee health professional
+    private String department; // department the professional belong to
 
     public HealthProfessional(int id, String name, String department) {
         this.id = id;
         this.name = name;
         this.department = department;
     }
-
+    //print the details of the health professional
     public void printDetails() {
         System.out.println("ID: " + id);
         System.out.println("Name: " + name);
         System.out.println("Department: " + department);
     }
-
+    //getter fo the name
+    //used when printing appointment details
     public String getName() {
         return name;
     }
-
+    //getter for the department
+    //used when printing appointment details
     public String getDepartment() {
         return department;
     }
